922e6cb6b6223c3f85c81cbadc27a25361965b72
Yash <lodhad@gmail.com>
2016-07-26 17:20:54 -0700

    Save

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$dbServername = "localhost";
+$dbName = "MyWebsite";
+$dbUsername = "MyWebsite";
+$dbPassword = "password";
+
+$siteTitle = "Devyash Everything!";
+$siteLogo = "assets/icons/dl_comp.png";
+
+$bodyBgColor = "white";
+$bodyTextColor = "black";
+
+$menuBgColor = "black";
+$menuTextColor = "white";
+
+$slideoutMenuBgColor = "#3D3196";
+
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require("config.php");
+
+$SqlConnection = NULL;
+try {
+  $SqlConnection = new PDO("mysql:host=" . $dbServername . ";dbname=" . $dbName, $dbUsername, $dbPassword);
+  $SqlConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+  die("Connection Failed: " . $e->getMessage());
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $siteTitle; ?></title>
+    <link href="style/main.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="header">
+      <div id="logo"></div>
+      <div id="menu"></div>
+    </div>
+    <div id="menu">&nbsp;</div>
+  </body>
+</html>
