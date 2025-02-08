da4cb0b88ffb003f856584813f152b242924613c
yash101 <lodhad@gmail.com>
2025-02-07 23:32:24 -0700

    Use default monaco CDN

--- src/components/widgets/CodeRunner/MonacoEditor.tsx
+++ src/components/widgets/CodeRunner/MonacoEditor.tsx
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
 import MonacoEditor, { EditorProps, loader } from '@monaco-editor/react';
 
-// const MonacoEditor = dynamic(() => import("@monaco-editor/react"), { ssr: false });
-
-loader.config({
-  paths: {
-    vs: '/monaco/vs'
-  }
-});
+// loader.config({
+//   paths: {
+//     vs: '/monaco/vs'
+//   }
+// });
 
 const LocalMonacoEditor: React.FC<EditorProps> = (props) => {
   return (
